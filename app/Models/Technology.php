95aa34789c204c9742d001b5ccbf05f1d8d70dec
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-31 15:27:35 +0200

    projects belongs to many in technologies

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'color'];
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
 }
